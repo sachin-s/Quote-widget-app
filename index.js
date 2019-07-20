6ba839c34d4e375d97de5c4c70cdf7bbc3bc6f45
Sachin S <sachins1221@gmail.com>
2019-07-20 16:40:00 +0530

    ui v 1

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 let request = require('request');
-request("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",function(err,response,body){
-    let bodyJson = JSON.parse(body);
-    let randomQuote = bodyJson[0]["content"];
-    document.getElementById("quote").innerHTML = randomQuote;
+const changeBtn = document.getElementById('changeBtn');
 
-});
-setInterval(function(){
+function myFunc(){
     var rnd = new Date().getTime();
     var url ="https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_="+rnd;
     request(url,function(err,response,body){
-    console.log(url);
     let bodyJson = JSON.parse(body);
     let randomQuote = bodyJson[0]["content"];
     document.getElementById("quote").innerHTML = randomQuote;
 
 });
 
-},5000)
+}
+myFunc();
+var myTimer = setInterval(myFunc,10000)
+changeBtn.addEventListener('click',function(){
+    clearInterval(myTimer);
+    myFunc();
+    myTimer = setInterval(myFunc,10000)
+})
