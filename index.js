24a7a53fc9c302ba527329dea0058796803da98b
Sachin S <sachins1221@gmail.com>
2019-07-21 11:57:09 +0530

    network availability checker added

--- index.js
+++ index.js
@@ -8,9 +8,15 @@ function myFunc(){
     var rnd = new Date().getTime();
     var url ="https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_="+rnd;
     request(url,function(err,response,body){
+    if(err)
+    {
+        document.getElementById("quote").innerHTML = 'Unable to fetch the quote plaese check the network connection';
+        return;
+    }
     let bodyJson = JSON.parse(body);
     let randomQuote = bodyJson[0]["content"];
     document.getElementById("quote").innerHTML = randomQuote;
+    
 
 });
 
