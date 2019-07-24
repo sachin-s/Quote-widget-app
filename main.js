dc02db636f501647a3a33dc0ce0c40012ba73480
Sachin S <sachins1221@gmail.com>
2019-07-24 20:32:06 +0530

    Application menu usage learnt

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const url = require("url");
 const ipc = electron.ipcMain;
+const Menu = electron.Menu;
 let win;
 console.log("Running from main.js");
 function createWindow() {
     winone = new BrowserWindow({
-        height:300,width:500,frame:false,
+        //height:300,width:500,frame:false,
           webPreferences:{
               nodeIntegration:true
         },show:false
@@ -31,7 +32,44 @@ function createWindow() {
    
 }
 
-app.on('ready', createWindow);
+app.on('ready', function(){
+    createWindow();
+    const template = [
+        {
+        label:'demo',
+        submenu:[
+            {
+                label:'submenu1',
+                click:function(){
+                    console.log('you clicked submenu1');
+                }
+            },
+            {
+                label:'submenu2',
+                click:function(){
+                    console.log('you clicked submenu2');
+                }
+            },
+            {type:'separator'},
+            {
+                label:'help',
+                click:function(){
+                    electron.shell.openExternal('https://electronjs.org/');
+                }
+            },
+            {
+                label:'exit',
+                click:function(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+   
+]
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+});
 app.on('window-all-closed', () => {
     if (process.platform != 'dawin') {
         app.quit()
